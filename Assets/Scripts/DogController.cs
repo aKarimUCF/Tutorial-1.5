583ec4f90df2e3daff8a26f7eff721c2e1cd53ab
unknown <akarim0517@gmail.com>
2021-10-03 15:10:19 -0400

    Animation commit

--- Assets/Scripts/DogController.cs
+++ Assets/Scripts/DogController.cs
@@ -10,12 +10,14 @@ public class DogController : MonoBehaviour
 
     public AudioSource musicSource;
 
-
+    Animator anim;
 
 
     // Start is called before the first frame update
     void Start()
-    {}
+    {
+      anim = GetComponent<Animator>();
+    }
 
     // Update is called once per frame
     void Update()
@@ -24,25 +26,26 @@ public class DogController : MonoBehaviour
         {
           musicSource.clip = musicClipOne;
           musicSource.Play();
-
+          anim.SetInteger("State", 1);
         }
 
         if (Input.GetKeyUp(KeyCode.W))
         {
           musicSource.Stop();
-
+          anim.SetInteger("State", 0);
         }
 
         if (Input.GetKeyDown(KeyCode.R))
         {
           musicSource.clip = musicClipTwo;
           musicSource.Play();
+          anim.SetInteger("State", 2);
         }
 
         if (Input.GetKeyUp(KeyCode.R))
         {
           musicSource.Stop();
-
+          anim.SetInteger("State", 0);
         }
 
         if (Input.GetKeyDown(KeyCode.L))
@@ -53,6 +56,11 @@ public class DogController : MonoBehaviour
         if (Input.GetKeyUp(KeyCode.L))
         {
           musicSource.loop = false;
+        }
+       
+        if (Input.GetKey("escape"))
+        {
+          Application.Quit();
         }
     }
 }
